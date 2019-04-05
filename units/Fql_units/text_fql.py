6f98c37e5a81689a7706e0619e47c8c9816fae86
aohanseven <449187400@qq.com>
2019-04-05 18:09:01 +0800

    新的测试功能

--- units/Fql_units/text_fql.py
+++ units/Fql_units/text_fql.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
+from HtmlTestRunner import HTMLTestRunner
 import unittest
 import requests
 import time
-
 
 def get_web():
     r = requests.get('http://fangqianli.com')
@@ -54,13 +54,17 @@ def ai_select(browser):
             browser.switch_to_window(i)
     browser.find_element_by_xpath('//*[@id="mapBox"]/div[4]/div[2]/button').click()
     time.sleep(1)
-    now1_windows = browser.current_window_handle
-    for i in all_windows:
-        if i != now_windows and i != now1_windows:
-            browser.switch_to_window(i)
-    time.sleep(10)
-    print(browser.find_element_by_xpath('//*[@id="app"]/div[2]/section[1]/div/div[1]/h2/text()'))
-    return 1
+    all_windows = browser.window_handles
+    browser.switch_to_window(all_windows[2])
+    time.sleep(3)
+    for i in browser.find_elements_by_xpath('//*[@id="app"]/div[2]/section[3]/div[1]/div/div/div[1]'):
+        if i.is_displayed()==False:
+            is_ok = 0
+            break
+        else:
+            is_ok = 1
+
+    return is_ok
 
 
 class TestFql(unittest.TestCase):
@@ -71,7 +75,7 @@ class TestFql(unittest.TestCase):
     def setUp(self):
         self.opt = webdriver.ChromeOptions()
         self.opt.set_headless()
-        self.browser = webdriver.Chrome('C:/Users/Administrator/Downloads/chromedriver.exe')
+        self.browser = webdriver.Chrome('C:/Users/Administrator/Downloads/chromedriver.exe',options=self.opt)
         self.browser.implicitly_wait(30)  # 隐性等待时间为30秒
         self.browser.get('http://fangqianli.com')
 
@@ -85,5 +89,25 @@ class TestFql(unittest.TestCase):
         self.browser.quit()
 
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    testunit = unittest.TestSuite()
+
+    testunit.addTest(TestFql("test_get_web"))
+
+    testunit.addTest(TestFql("test_login"))
+
+    testunit.addTest(TestFql("test_ai_select"))
+
+    HtmlFile = "E:/Fql_Project/units/HTML_test/report.html"
+
+    fp = open(HtmlFile, "wb")
+
+    runner = HTMLTestRunner.HTMLTestRunner(
+        stream=fp,
+        title='房千里测试报告',
+        description='用例执行情况：'
+        )
+
+    runner.run(testunit)
+
+    fp.close()
