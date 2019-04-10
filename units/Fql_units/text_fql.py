0fb4cb11b73434bcdbfded5aa201b3658b62194a
aohanseven <449187400@qq.com>
2019-04-10 10:48:49 +0800

    房千里网站基础功能测试完成

--- units/Fql_units/text_fql.py
+++ units/Fql_units/text_fql.py
@@ -1,8 +1,38 @@
 from selenium import webdriver
-from HtmlTestRunner import HTMLTestRunner
 import unittest
 import requests
 import time
+import re
+import json
+
+def get_token():
+    url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+    data = {
+        "corpid": 'ww1bfe0036f1ec1e74',
+        "corpsecret": '-92Yv4C7KaU4AUeDRm1SoRxQy44mVTQKZj6lxZfp5lA'
+
+    }
+    r = requests.get(url=url, params=data, verify=False)
+    token = r.json()['access_token']
+    return token
+
+
+def send_msg(token,content):
+    url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % token
+    data = {
+        "touser": '@all',                                # 企业号中的用户帐号，账号名
+        "msgtype": "text",                               # 消息类型。
+        "agentid": '1000026',                              # 企业号中的应用id。
+        "text": {
+            "content": content
+        },
+        "safe": "0"
+    }
+    r = requests.post(url=url, data=json.dumps(data), verify=False)
+    return r.text
+
+
+
 
 def get_web():
     r = requests.get('http://fangqianli.com')
@@ -20,9 +50,9 @@ def login(browser):
     browser.find_element_by_xpath('//*[@id="loginbtn"]').click()
     time.sleep(2)
     if browser.find_element_by_link_text('16608940910'):
-        return True
+        return 0
     else:
-        return False
+        return 1
 
 
 def ai_select(browser):
@@ -57,20 +87,20 @@ def ai_select(browser):
     all_windows = browser.window_handles
     browser.switch_to_window(all_windows[2])
     time.sleep(3)
+    is_ok = ''
     for i in browser.find_elements_by_xpath('//*[@id="app"]/div[2]/section[3]/div[1]/div/div/div[1]'):
         if i.is_displayed()==False:
-            is_ok = 0
+            is_ok = 1
             break
         else:
-            is_ok = 1
-
+            is_ok = 0
     return is_ok
 
 
 class TestFql(unittest.TestCase):
 
     def test_get_web(self):
-        self.assertEqual(200, get_web())
+        self.assertEqual(200, get_web(), msg="主页无法访问")
 
     def setUp(self):
         self.opt = webdriver.ChromeOptions()
@@ -80,34 +110,29 @@ class TestFql(unittest.TestCase):
         self.browser.get('http://fangqianli.com')
 
     def test_login(self):
-        self.assertTrue(login(self.browser))
+        self.assertEqual(0, login(self.browser), msg="登录系统错误")
 
     def test_ai_select(self):
-        self.assertEqual(1, ai_select(self.browser))
+        self.assertEqual(0, ai_select(self.browser), msg="选址功能出错")
 
     def tearDown(self):
         self.browser.quit()
 
 
 if __name__ == "__main__":
+
+    runner = unittest.TextTestRunner()
     testunit = unittest.TestSuite()
-
     testunit.addTest(TestFql("test_get_web"))
-
     testunit.addTest(TestFql("test_login"))
-
     testunit.addTest(TestFql("test_ai_select"))
-
-    HtmlFile = "E:/Fql_Project/units/HTML_test/report.html"
-
-    fp = open(HtmlFile, "wb")
-
-    runner = HTMLTestRunner.HTMLTestRunner(
-        stream=fp,
-        title='房千里测试报告',
-        description='用例执行情况：'
-        )
-
-    runner.run(testunit)
-
-    fp.close()
+    result = runner.run(testunit)
+    failures = result.failures
+    msg = []
+    if failures != []:
+        for i, k in zip(failures,range(len(failures))):
+            msg.append('错误{:d}:'.format(k+1) + re.search('msg=\"(.*?)\"', i[1]).group(1))
+    if msg != []:
+        content = ','.join(msg)
+        token = get_token()
+        send_msg(token,content)
